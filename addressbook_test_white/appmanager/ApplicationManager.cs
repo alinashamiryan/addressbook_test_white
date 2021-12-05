145c33d725572e70314cceb82166d3937a0f2d2c
Шамирян Алина Александровна <A.Shamiryan@norbit.ru>
2021-12-05 15:02:52 +0300

    Добавьте файлы проекта.

---
+++ addressbook_test_white/appmanager/ApplicationManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestStack.White;
+using TestStack.White.UIItems.WindowItems;
+using TestStack.White.UIItems;
+
+namespace addressbook_test_white
+{
+   public class ApplicationManager
+    {
+        public static string WINTITLE = "Free Address Book";
+        private GroupHelper groupHelper;
+        public ApplicationManager()
+        {
+            Application app = Application
+                .Launch(@"c:\Users\A.Shamiryan\Desktop\Алина\FreeAddressBookPortable\AddressBook.exe");
+            MainWindow = app.GetWindow(WINTITLE);  
+            groupHelper = new GroupHelper(this);
+        }
+        public void Stop()
+        {
+            MainWindow.Get<Button>("uxExitAddressButton").Click();
+        }
+        
+        public Window MainWindow { get; private set; }
+        public GroupHelper Groups
+        {
+            get { return groupHelper; }
+        }
+    }
+}
---
+++ addressbook_test_white/appmanager/GroupHelper.cs
@@ -0,0 +1,87 @@
+﻿
+using System;
+using System.Collections.Generic;
+using TestStack.White;
+using TestStack.White.WindowsAPI;
+using TestStack.White.UIItems.WindowItems;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.TreeItems;
+using TestStack.White.UIItems.Finders;
+using System.Windows.Automation;
+using TestStack.White.InputDevices;
+
+namespace addressbook_test_white
+{
+    public class GroupHelper:HelperBase
+    {
+        public static string GROUPWINTITLE = "Group editor";
+        public static string DELGROUPWINTITLE = "Delete group";
+        public GroupHelper(ApplicationManager manager): base(manager) { }
+
+        public List<GroupData> GetGroupList()
+        {
+            List<GroupData> list = new List<GroupData>();
+            Window dialogue = OpenGroupsDialogue();
+            Tree tree = dialogue.Get<Tree>("uxAddressTreeView");
+            TreeNode root = tree.Nodes[0];
+            foreach(TreeNode item in root.Nodes) 
+            {
+                list.Add(new GroupData()
+                {
+                    Name = item.Text
+                });
+            }
+            ClouseGroupsDialogue(dialogue);
+            return list;
+
+        }
+
+        public void Add(GroupData newGroup)
+        {
+            Window dialogue = OpenGroupsDialogue();
+            InitGruopCreatin(dialogue);
+            TextBox textBox = (TextBox)dialogue.Get(SearchCriteria.ByControlType(ControlType.Edit));
+            textBox.Enter(newGroup.Name);
+            Keyboard.Instance.PressSpecialKey(KeyboardInput.SpecialKeys.RETURN);
+            ClouseGroupsDialogue(dialogue);
+        }
+        public void Remove(int index)
+        {
+            Window dialogue = OpenGroupsDialogue();
+            SelectGroup(index, dialogue);
+            Delete(dialogue);
+            ClouseGroupsDialogue(dialogue);
+        }
+
+        public void Delete(Window dialogue)
+        {
+            dialogue.Get<Button>("uxDeleteAddressButton").Click();
+            Window dialogue2 = dialogue.ModalWindow(DELGROUPWINTITLE);
+            dialogue2.Get<Button>("uxOKAddressButton").Click();
+        }
+
+        public void SelectGroup(int index, Window dialogue)
+        {
+            Tree tree = dialogue.Get<Tree>("uxAddressTreeView");
+            TreeNode root = tree.Nodes[0];
+            root.Nodes[0].Select();
+
+        }
+
+        public static void InitGruopCreatin(Window dialogue)
+        {
+            dialogue.Get<Button>("uxNewAddressButton").Click();
+        }
+
+        public void ClouseGroupsDialogue(Window dialogue)
+        {
+            dialogue.Get<Button>("uxCloseAddressButton").Click();
+        }
+
+        public Window OpenGroupsDialogue()
+        {
+           manager.MainWindow.Get<Button>("groupButton").Click();
+           return manager.MainWindow.ModalWindow(GROUPWINTITLE);
+        }
+    }
+}
